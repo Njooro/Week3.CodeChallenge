5365e535e73b7af9f7db8c20bebe18afc4ae3778
Maxwell <njoromax5@gmail.com>
2023-10-24 17:11:28 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+
+document.addEventListener('DOMContentLoaded', function (){
+    
+    const placeMovie = () => {
+        fetch("http://localhost:3000/films")
+        .then(res => res.json())
+        .then(data =>{
+           console.log(data)
+            if (data.films && data.films.length > 0){
+            const firstMovie = data.films[0]
+            const filmImage = document.getElementById("poster");
+            const movieTitle = document.getElementById("movieTitle");
+            const movieDescription = document.getElementById("movieDescription");
+            const runningTime = document.getElementById("runTime");
+            const showingTime = document.getElementById("showTime");
+            const availableTicket = document.getElementById("ticketsAvailable");
+            filmImage.src = firstMovie.poster;
+            movieTitle.innerText = firstMovie.title;
+            movieDescription.innerText = firstMovie.description;
+            runningTime.innerText = `Runtime: ${firstMovie.runtime} minutes`;
+            showingTime.innerText =  `Showtime: ${firstMovie.showtime}`;
+            availableTicket.innerText = `Tickets Available: (${firstMovie.capacity - firstMovie.tickets_sold})`;
+            const ticketBuy = document.getElementById("buyTicket");
+            let tickets = Number(firstMovie.capacity - firstMovie.tickets_sold)
+
+            ticketBuy.addEventListener('click',()=>{
+
+                tickets--
+
+                // const ticketRemaining = tickets-1
+
+                if(tickets <= 0){
+                    const frstMovie = document.getElementById("1")
+                    frstMovie.innerHTML=`${firstMovie.title}  <span class="badge bg-danger me-1">SOLD OUT</span>`
+
+                    availTicket.innerHTML = `Ticketd available:  <span class="badge bg-danger">SOLD OUT</span>`
+                }else{
+                    availTicket.innerText = `Tickets available: (${tickets})`
+                }
+                
+            })
+
+
+            }
+        })
+
+        
+    }
+function showMovieDetails(){
+    fetch('http://localhost:3000/films')
+    .then(data => data.json())
+    .then(response =>{
+        filmData = [...response];
+        console.log(filmData)
+        for (let i =0; i < filmData.length; i++){
+            let item = filmData[i];
+            console.log(item)
+            const movieList = document.createElement("li");
+            const list = document.getElementById("showingMovie");
+            movieList.classList.add("list-item", "border", "border-info", "sinema" );
+            movieList.setAttribute('id', `${item.id}`);
+            movieList.innerText = item.title;
+            console.log(item.title);
+            list.appendChild(movieList);
+        }
+    })
+}
+showMovieDetails()
+ placeMovie()
+})
